a6679aee3a7fc383f25bcef810ed4fdef0549997
Kasra Amani <kasra138079@gmail.com>
2023-04-16 22:24:32 +0330

    q1

--- HW1/q1.cpp
+++ HW1/q1.cpp
@@ -1,1 +1,22 @@
-#include <>
+#include <iostream>
+
+using namespace std;
+
+int main (){
+    int size = 0;
+    cin >> size;
+    long long total = 0;
+    long int temp;
+    for (int i=0; i<size; i++){
+        cin >> temp;
+        total += temp-1;
+        if (total%2 == 0){
+            cout << '2' << endl;
+            total = 2;
+        }else {
+            cout << '1' << endl;
+            total = 1;
+        }
+    }
+    return 0;
+}
